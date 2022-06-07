63b70654d370b28a8f613595d04c90bdf79d7eb4
wiut00012426 <bahronovbahtiyor4@gmail.com>
2022-06-07 16:19:14 +0500

    cuisine-app

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Home from "./Home";
+import Searched from "./Searched";
+import Recipe from "./Recipe";
+import Cuisine from "./Cuisine";
+import { Route, Routes, useLocation } from "react-router-dom"; 
+import { AnimatePresence } from "framer-motion";
+
+function Pages() {
+  const location = useLocation();
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <Routes location={location} key={location.id}>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine/:type" element={<Cuisine />} />
+        <Route path="/searched/:search" element={<Searched />} />
+        <Route path="/recipe/:name" element={<Recipe />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+export default Pages;
